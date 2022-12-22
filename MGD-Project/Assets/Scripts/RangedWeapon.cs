97b974ee9fefd6daaef816a199b0843083a2f8a3
Merv39 <78385901+Merv39@users.noreply.github.com>
2022-12-22 02:23:57 +0000

    Fix: projectile shoots based on mouse position
    uses mouse position rather than weapon rotation which was inaccurate

--- MGD-Project/Assets/Scripts/RangedWeapon.cs
+++ MGD-Project/Assets/Scripts/RangedWeapon.cs
@@ -26,8 +26,24 @@ public class RangedWeapon : MonoBehaviour
         {
             if (cooldownCheck())
             {
+                //shoot at cursor position
+                //rotate projectile to look at the current mouse position
+                RaycastHit lookHit;
+                Vector3 direction;
+
+                if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out lookHit, 100)) //only changes direction if pointed at a surface
+                {
+                    Vector3 finalPoint = new Vector3(lookHit.point.x, 0, lookHit.point.z);
+                    Vector3 difference = finalPoint - transform.position;  //direction is the difference between weapon pos and point pos
+                    direction = difference.normalized;
+                    direction = new Vector3(direction.x, 0, direction.z);
+                }
+                else {
+                    direction = projectileSpawnPoint.forward;
+                }
                 var projectile = Instantiate(projectilePrefab, projectileSpawnPoint.position, projectileSpawnPoint.rotation);
-                projectile.GetComponent<Rigidbody>().velocity = projectileSpawnPoint.forward * projectileSpeed;
+                //projectile.GetComponent<Rigidbody>().velocity = projectileSpawnPoint.forward * projectileSpeed;
+                projectile.GetComponent<Rigidbody>().velocity = direction * projectileSpeed;
             }
         }
     }
