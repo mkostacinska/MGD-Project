0d993fb1f6c0786a182427b28e0ec2750ad89d1a
Merv39 <78385901+Merv39@users.noreply.github.com>
2023-01-04 21:41:26 +0000

    fix: weapon pickup

--- MGD-Project/Assets/Scripts/PickupItem.cs
+++ MGD-Project/Assets/Scripts/PickupItem.cs
@@ -9,6 +9,7 @@ public class PickupItem : PickupController
 
     //override method so the item goes into player inventory
     new private void Start(){
+        if (player == null) {player = PlayerToFollow.shared.player; } //prevents breaking other scenes
         text = Instantiate(labelPrefab);
         base.Start();       //get the start() from superclass
 
@@ -42,7 +43,7 @@ public class PickupItem : PickupController
     void checkDistance()
     {
         //if the player is within 1.5 unit from the item, display the prompt 
-        if (Vector3.Distance(transform.position, PlayerToFollow.shared.player.transform.position) <= 1.5f)
+        if (Vector3.Distance(transform.position, player.transform.position) <= 1.5f)
         {
             text.SetActive(true);
             if (keyDown)
