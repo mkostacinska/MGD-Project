a7282c926701073b466a6d6bd03edfa14008a207
Merv39 <78385901+Merv39@users.noreply.github.com>
2023-01-06 20:13:58 +0000

    Minor Refactor

--- MGD-Project/Assets/Scripts/SlowTime.cs
+++ MGD-Project/Assets/Scripts/SlowTime.cs
@@ -4,25 +4,30 @@ using UnityEngine;
 
 public class SlowTime : MonoBehaviour
 {
-    public static void slowTime(float seconds, float amount)
+    public static void slowTime(float seconds, float amount = 0f, bool screen = true)
     {
-        GameObject.FindGameObjectsWithTag("SlowTime")[0].GetComponent<SlowTime>().slowTimeNonStatic(seconds, amount);
+        GameObject.FindGameObjectsWithTag("SlowTime")[0].GetComponent<SlowTime>().slowTimeNonStatic(seconds, amount, screen);
+    }
+    public static void slowTime(float seconds, bool screen) //method overloading
+    {
+        slowTime(seconds, 0f, screen = true);
     }
 
     //method to slow time: pass in seconds and amount; freezes time by default if only seconds specified
-    public void slowTimeNonStatic(float seconds, float amount = 0f) { StartCoroutine(iSlowTime(seconds, amount)); }
+    public void slowTimeNonStatic(float seconds, float amount = 0f, bool screen = true) { StartCoroutine(iSlowTime(seconds, amount, screen)); }
 
     [SerializeField] private GameObject freezeMenu;
-    public IEnumerator iSlowTime(float seconds, float amount)
+    private IEnumerator iSlowTime(float seconds, float amount, bool screen)
     {
         //When timescale = 1: game is running at normal speed
         //if amount is 0: game is frozen
         Time.timeScale = amount;    //set the game speed
-        GameObject freezeScreen = Instantiate(freezeMenu);
-        print("freezing");
+        GameObject freezeScreen = null;
+        if (screen == true) { freezeScreen = Instantiate(freezeMenu); }
 
         yield return new WaitForSecondsRealtime(seconds);
+
         Time.timeScale = 1f;    //resume time
-        Destroy(freezeScreen);
+        if (screen == true) { Destroy(freezeScreen); }
     }
 }
