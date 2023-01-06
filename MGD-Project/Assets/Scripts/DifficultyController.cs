90cffd1141b4ea011a35d9629a69c9c0cd49d5c3
Merv39 <78385901+Merv39@users.noreply.github.com>
2023-01-06 00:26:06 +0000

    feat: Add a difficulty mode slider into the options in the menu

---
+++ MGD-Project/Assets/Scripts/DifficultyController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DifficultyController : MonoBehaviour //modification of VolumeController
+{
+    [SerializeField] private Slider DifficultySliderUI = null;//UI slider
+    [SerializeField] private Text DifficultyText = null;//UI text
+    private int defaultDifficulty = 1;
+    private int difficultyValue;
+
+    private void Start()
+    {
+        LoadValues();
+        DifficultySlider(); //set initial text before player moves the slider
+    }
+
+    public void DifficultySlider()  //triggers whenever the slider moves
+    {
+        SaveDifficulty();
+        string text = "Default";
+        switch (difficultyValue)
+        {
+            case 0:
+                text = "Easy";
+                break;
+
+            case 1:
+                text = "Medium";
+                break;
+
+            case 2:
+                text = "Hard";
+                break;
+        }
+        DifficultyText.text = text;//UI text
+    }
+
+    public void SaveDifficulty()
+    {
+        //Debug.Log("Saved");
+        difficultyValue = (int) DifficultySliderUI.value; //slider position determines difficulty, expects an int
+        PlayerPrefs.SetInt("gameDifficulty", difficultyValue);
+        LoadValues();
+
+    }
+
+    void LoadValues()
+    {
+        difficultyValue = PlayerPrefs.GetInt("gameDifficulty", defaultDifficulty);    //sets the difficulty to default difficulty if no player prefs exist
+        DifficultySliderUI.value = difficultyValue;   //sets the slider value
+        //call an external script to set the difficulty of game here
+        //pass in difficulty value which is an int
+    }
+
+
+}
